e500d2982234c4cfd7b9d275c71b5cbfa0a7afab
sabinopa <priasabino@gmail.com>
2024-04-15 22:30:01 -0300

    build: fornecedor vê formulario para criar empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,7 @@ class CompaniesController < ApplicationController
   before_action :authenticate_supplier!, only: [:new, :create]
 
   def new
+    @company = Company.new
   end
 
   def create
--- spec/system/company/supplier_creates_company_spec.rb
+++ spec/system/company/supplier_creates_company_spec.rb
@@ -21,4 +21,34 @@ describe 'Supplier creates company' do
 
     expect(current_path).to eq new_company_path
   end
+
+  it 'and sees the fields to create a company' do
+    PaymentMethod.create!(method: 'PIX')
+    PaymentMethod.create!(method: 'Cartão de Crédito')
+    PaymentMethod.create!(method: 'Cartão de Débito')
+    PaymentMethod.create!(method: 'Dinheiro')
+    supplier = Supplier.create!(name: 'Priscila', lastname: 'Sabino', email: 'priscila@email.com', password: '12345678')
+
+    login_as(supplier, :scope => :supplier)
+    visit new_company_path
+
+    expect(page).to have_content 'Cadastre sua empresa'
+    expect(page).to have_field 'Nome Fantasia'
+    expect(page).to have_field 'Razão Social'
+    expect(page).to have_field 'CNPJ'
+    expect(page).to have_field 'Telefone'
+    expect(page).to have_field 'E-mail'
+    expect(page).to have_field 'Endereço'
+    expect(page).to have_field 'Bairro'
+    expect(page).to have_field 'Cidade'
+    expect(page).to have_field 'Estado'
+    expect(page).to have_field 'CEP'
+    expect(page).to have_field 'Descrição'
+    expect(page).to have_content 'Métodos de pagamentos aceitos'
+    expect(page).to have_content 'PIX'
+    expect(page).to have_content 'Cartão de Crédito'
+    expect(page).to have_content 'Cartão de Débito'
+    expect(page).to have_content 'Dinheiro'
+    expect(page).to have_button 'Salvar'
+  end
 end
