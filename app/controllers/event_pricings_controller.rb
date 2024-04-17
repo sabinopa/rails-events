b1ce4db04da37d78845c477f5ff5969fb1ff9fb4
sabinopa <priasabino@gmail.com>
2024-04-17 13:41:05 -0300

    feat: fornecedor cria preço do evento com sucesso

--- app/controllers/event_pricings_controller.rb
+++ app/controllers/event_pricings_controller.rb
@@ -2,7 +2,9 @@ class EventPricingsController < ApplicationController
   before_action :authenticate_supplier!, only: [:new, :create]
 
   def show
-
+    @event_pricing = EventPricing.find(params[:id])
+    @event_type = @event_pricing.event_type
+    @company = @event_type.company
   end
 
   def new
@@ -11,6 +13,21 @@ class EventPricingsController < ApplicationController
   end
 
   def create
+    @event_type = EventType.find(params[:event_type_id])
+    @event_pricing = @event_type.event_pricings.new(event_pricing_params)
+    if @event_pricing.save
+      flash[:notice] = t('.success', name: @event_type.name)
+      redirect_to event_pricing_path(@event_pricing)
+    else
+      flash.now[:alert] = t('.error')
+      render :new
+    end
+  end
 
+  private
+
+  def event_pricing_params
+    params.require(:event_pricing).permit(:event_type_id, :base_price, :base_attendees,
+                                          :additional_guest_price, :extra_hour_price, :weekend)
   end
 end
--- spec/system/event_pricing/supplier_creates_event_pricing_spec.rb
+++ spec/system/event_pricing/supplier_creates_event_pricing_spec.rb
@@ -74,4 +74,37 @@ describe 'Supplier creates event pricing' do
     expect(page).to have_field 'Dias Úteis'
     expect(page).to have_button 'Salvar'
   end
+
+  it 'successfully' do
+    pix = PaymentMethod.create!(method: 'PIX')
+    credito = PaymentMethod.create!(method: 'Cartão de Crédito')
+    debito = PaymentMethod.create!(method: 'Cartão de Débito')
+    supplier = Supplier.create!(name: 'Priscila', lastname: 'Sabino', email: 'priscila@email.com', password: '12345678')
+    company = Company.create!(supplier_id: supplier.id, brand_name: 'Estrelas Mágicas', corporate_name: 'Estrelas Mágicas Buffet Infantil Ltda',
+                            registration_number: '12.333.456/0001-78',  phone_number: '(11) 2233-4455', email: 'festas@estrelasmagicas.com.br',
+                            address: 'Alameda dos Sonhos, 404', neighborhood: 'Vila Feliz', city: 'São Paulo', state: 'SP', zipcode: '05050-050',
+                            description: 'O Estrelas Mágicas é especializado em trazer alegria e diversão para festas infantis.')
+                            company.payment_methods << [pix, credito, debito]
+    event_type = EventType.create!(company_id: company.id, name: 'Festa Temática de Piratas',
+                                  description: 'Uma aventura inesquecível pelos Sete Mares! Nossa Festa Temática de Piratas inclui caça ao tesouro, decoração temática completa, e muita diversão para os pequenos aventureiros.',
+                                  min_attendees: 20, max_attendees: 50, duration: 240,
+                                  menu_description: 'Cardápio temático com mini-hambúrgueres, batatas em forma de joias, sucos naturais e bolo do tesouro. Opções vegetarianas disponíveis.',
+                                  alcohol_available: false, decoration_available: true, parking_service_available: true, location_type: 1)
+    login_as(supplier, :scope => :supplier)
+    visit new_event_type_event_pricing_path(event_type)
+
+    choose 'Dias Úteis'
+    fill_in 'Preço Base', with: 900.00
+    fill_in 'Preço por Convidado Adicional', with: 30.00
+    fill_in 'Número Base de Convidados', with: 50
+    fill_in 'Preço por Hora Extra', with: 75.00
+    click_on 'Salvar'
+
+    expect(current_path).to eq event_pricing_path(event_type)
+    expect(page).to have_content 'Novo preço criado com sucesso!'
+    expect(page).to have_content 'Preço para Dias Úteis'
+    expect(page).to have_content 'Preço Base: 900.0'
+    expect(page).to have_content 'Número Base de Convidados: 50'
+    expect(page).to have_content 'Preço por Hora Extra: 75.0'
+  end
 end
