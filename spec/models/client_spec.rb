07aaeb00e91c0831a313cfce0b246bec3171fc0d
sabinopa <priasabino@gmail.com>
2024-04-30 13:59:04 -0300

    spec: teste de modelo client

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,4 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
+  describe '#description' do
+    it 'show name, lastname and email' do
+      client = Client.new(name: 'Sabrina', lastname: 'Oliveira',document_number: '525.202.290-98',
+                              email: 'sabrina@email.com', password: '12345678')
+
+      result = client.description
+
+      expect(result).to eq 'Sabrina Oliveira - sabrina@email.com'
+    end
+  end
+
+  describe '#valid?' do
+    it 'returns false when name is empty' do
+      client = Client.new(name: '', lastname: 'Oliveira', document_number: '525.202.290-98',
+                              email: 'sabrina@email.com', password: '12345678')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'returns false when lastname is empty' do
+      client = Client.new(name: 'Sabrina', lastname: '', document_number: '525.202.290-98',
+                              email: 'sabrina@email.com', password: '12345678')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'returns false when document number is empty' do
+      client = Client.new(name: 'Sabrina', lastname: 'Oliveira', document_number: '',
+                              email: 'sabrina@email.com', password: '12345678')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'returns false when email is empty' do
+      client = Client.new(name: 'Sabrina', lastname: 'Oliveira',document_number: '525.202.290-98',
+                              email: '', password: '12345678')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'returns false when password is empty' do
+      client = Client.new(name: 'Sabrina', lastname: 'Oliveira',document_number: '525.202.290-98',
+                              email: 'sabrina@email.com', password: '')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'returns false when document number is not valid' do
+      client = Client.new(name: 'Sabrina', lastname: 'Oliveira',document_number: '525202998',
+                              email: 'sabrina@email.com', password: '12345678')
+
+      result = client.valid?
+
+      expect(result).to eq false
+    end
+  end
 end
--- spec/system/company/supplier_sees_company_details_spec.rb
+++ spec/system/company/supplier_sees_company_details_spec.rb
@@ -37,6 +37,12 @@ describe 'Owner sees company details' do
                             address: 'Alameda dos Sonhos, 404', neighborhood: 'Vila Feliz', city: 'São Paulo', state: 'SP', zipcode: '05050-050',
                             description: 'O Estrelas Mágicas é especializado em trazer alegria e diversão para festas infantis.')
                             company.payment_methods << [pix, credito, debito]
+    event_type = EventType.create!(company_id: supplier.id, name: 'Festa Temática de Piratas',
+                            description: 'Uma aventura inesquecível pelos Sete Mares! Nossa Festa Temática de Piratas inclui caça ao tesouro, decoração temática completa, e muita diversão para os pequenos aventureiros.',
+                            min_attendees: 20, max_attendees: 50, duration: 240,
+                            menu_description: 'Cardápio temático com mini-hambúrgueres, batatas em forma de joias, sucos naturais e bolo do tesouro. Opções vegetarianas disponíveis.',
+                            alcohol_available: false, decoration_available: true, parking_service_available: true, location_type: 0)
+                            event_type.photos.attach(io: File.open(Rails.root.join('spec', 'files', 'festa_aniversario_decoracao.jpg')), filename: 'festa_aniversario_decoracao.jpg')
     login_as(supplier, :scope => :supplier)
     visit root_path
     within 'nav' do
@@ -44,13 +50,7 @@ describe 'Owner sees company details' do
     end
 
     expect(current_path).to eq company_path(company.id)
-    expect(page).to have_content 'Estrelas 45456456456456Mágicas'
-    expect(page).to have_content 'O Estrelas Mágicas é especializado em trazer alegria e diversão para festas infantis.'
-    expect(page).to have_content 'Estrelas Mágicas Buffet Infantil Ltda'
-    expect(page).to have_content '58.934.722/0001-01'
-    expect(page).to have_content 'Alameda dos Sonhos, 404'
-    expect(page).to have_content 'Vila Feliz, São Paulo - SP, 05050-050'
-    expect(page).to have_content 'PIX | Cartão de Crédito | Cartão de Débito'
+    expect(page).to have_css('img[src*="festa_aniversario_decoracao.jpg"]')
   end
 
   it 'and returns to homepage' do
